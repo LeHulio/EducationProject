6475832abea9d29954891a29e92c202188c2e3b1
Alex Lasitsa <alexmers911@mail.ru>
2021-09-22 14:35:38 +0300

    link commit to issue #1

--- ApplicationTest/src/lehulio/Application.java
+++ ApplicationTest/src/lehulio/Application.java
@@ -14,7 +14,16 @@ public class Application {
     public static void main(String... args) throws IOException {
         logger.info("info");
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = null;
+        int port;
+        if (args[0].length() > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+                server = HttpServer.create(new InetSocketAddress(port), 0);
+            } catch (NumberFormatException e) {
+                System.out.println("Should be a number");
+            }
+        }
         server.createContext("/articles", new PostsAndSearchHandler());
         server.setExecutor(null);
         server.start();
